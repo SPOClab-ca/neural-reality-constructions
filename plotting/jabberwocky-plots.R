028fe4ce142f64981f537109504869db625ff281
Bai Li <bai@cs.toronto.edu>
2021-10-05 18:59:46 -0400

    J/G colorful 4x4 tile plots

--- plotting/jabberwocky-plots.R
+++ plotting/jabberwocky-plots.R
@@ -14,7 +14,11 @@ high_freq_df = data.frame(
            11.924, 11.701, 11.868, 11.864,
            11.691, 11.593, 11.395, 11.599,
            11.740, 11.954, 11.936, 11.517)
-)
+) %>%
+  mutate(cxn=factor(cxn, levels=rev(c(
+    "ditransitive", "resultative", "caused-motion", "removal")))) %>%
+  mutate(verb=factor(verb, levels=c(
+    "gave", "made", "put", "took")))
 
 low_freq_df = data.frame(
   cxn=c("ditransitive", "ditransitive", "ditransitive", "ditransitive",
@@ -29,8 +33,38 @@ low_freq_df = data.frame(
            12.230, 12.466, 12.562, 13.095,
            11.791, 12.142, 11.906, 12.742,
            11.860, 12.420, 12.651, 12.246)
-)
+) %>%
+  mutate(cxn=factor(cxn, levels=rev(c(
+    "ditransitive", "resultative", "caused-motion", "removal")))) %>%
+  mutate(verb=factor(verb, levels=c(
+    "handed", "turned", "placed", "removed")))
 
-# WIP
+# fig:jg-grid-high-freq
 ggplot(high_freq_df, aes(verb, cxn, fill=values)) + 
-  geom_tile()
+  geom_tile() +
+  xlab("") +
+  ylab("") +
+  scale_x_discrete(position = "top") +
+  scale_fill_distiller(palette = "RdYlGn",
+    limits = c(min(high_freq_df$values) - 0.1, max(high_freq_df$values) + 0.1)) +
+  geom_text(aes(label = values), color="#303030") +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  theme(text = element_text(size=15))
+
+ggsave("jg-grid-high-freq.pdf", width=4.5, height=3.5)
+
+# fig: jg-grid-low-freq
+ggplot(low_freq_df, aes(verb, cxn, fill=values)) + 
+  geom_tile() +
+  xlab("") +
+  ylab("") +
+  scale_x_discrete(position = "top") +
+  scale_fill_distiller(palette = "RdYlGn",
+    limits = c(min(low_freq_df$values) - 0.1, max(low_freq_df$values) + 0.1)) +
+  geom_text(aes(label = values), color="#303030") +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  theme(text = element_text(size=15))
+
+ggsave("jg-grid-low-freq.pdf", width=4.5, height=3.5)
